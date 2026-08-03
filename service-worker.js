1910f81edbbe19a696ed43a5793fadb04027be56
nvthanh2208-web <nv.thanh2208@gmail.com>
2026-08-04 02:35:05 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "soc-hoc-chu-v16-auto-question-fireworks";
+const CACHE_NAME = "soc-hoc-chu-v4-am-thanh-rieng";
 
 const LETTER_CODES = [
   "a", "aw", "aa", "b", "c", "d", "dd", "e", "ee", "g",
